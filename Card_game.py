59c4411db3546075d43931d35cc4574c1c19e9e2
Parth Patel <Parth.Patel@dal.ca>
2019-02-04 11:07:33 -0400

    final solution

--- Card_game.py
+++ Card_game.py
@@ -1,6 +1,6 @@
 class card():
   """this creates a card object"""
-
+  
   suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades'] 
   rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
   
@@ -10,7 +10,8 @@ class card():
     self.suite =  suite
   
   def __str__(self):
-    return "the rank is {rank_names[self.rank]} and the suit is {suit_name[self.suit]}"
+  
+    return f"the rank is {card.rank_names[self.rank]} and the suit is {card.suit_names[self.suite]}"
 
 ace_of_spade = card(1,3)
 
